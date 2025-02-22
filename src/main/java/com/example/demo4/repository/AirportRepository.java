4e5dfa04f53e4d12f392bf44eef46ca60aeec414
pedroGEOGIScoding <pedromartinezduran@gmail.com>
2025-02-22 21:23:19 +0100

    Changes on Airport and Flight models

--- src/main/java/com/example/demo4/repository/AirportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo4.repository;
-
-import com.example.demo4.model.Airport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AirportRepository extends JpaRepository<Airport, String> {
-
-
-}
